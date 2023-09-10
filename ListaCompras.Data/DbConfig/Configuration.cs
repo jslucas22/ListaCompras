52ca347bc4c268cf89cbf714c94a00ff2907b9c6
jslucas22 <jose.esbellute@gmail.com>
2023-09-10 08:24:05 -0300

    Creating connection to MySQL Database

---
+++ ListaCompras.Data/DbConfig/Configuration.cs
@@ -0,0 +1,13 @@
+﻿using ListaCompras.Domain.Models.Database;
+using MySql.Data.MySqlClient;
+
+namespace ListaCompras.Data.DbConfig
+{
+    public class Configuration
+    {
+        public static MySqlConnection GetSqlConnection()
+        {
+            return new MySqlConnection(DbStaticDefaults.ConnectionString);
+        }
+    }
+}
